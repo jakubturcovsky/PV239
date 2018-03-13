8a11fa54e8e46625c9b7ecbd199093122c11b338
Jakub Turcovsky <jakub.turcovsky@dotykacka.cz>
2018-03-13 20:45:58 +0100

    TODOs

--- PV239/seminar4/src/main/java/cz/pv239/seminar4/activity/FileActivity.java
+++ PV239/seminar4/src/main/java/cz/pv239/seminar4/activity/FileActivity.java
@@ -1,17 +1,12 @@
 package cz.pv239.seminar4.activity;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -35,8 +30,6 @@ public class FileActivity
 
     private static final int REQUEST_WRITE_PERMISSION = 42;
 
-    private boolean mSaving;
-
     @BindView(R.id.content) EditText mContentEditText;
 
     @Override
@@ -49,49 +42,22 @@ public class FileActivity
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == REQUEST_WRITE_PERMISSION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (mSaving) {
-                    saveData();
-                }
-            } else {
-                Toast.makeText(FileActivity.this, "Couldn't save data", Toast.LENGTH_SHORT).show();
-            }
+            // TODO: if permission was granted, save file
         }
     }
 
     @OnClick(R.id.save)
     protected void onSaveClicked() {
-        mSaving = true;
         writePermissionCheck();
     }
 
     private void writePermissionCheck() {
-        // runtime permissions start with the Marshmallow ->
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            // Lower than Marshmallow -> permissions were granted during the install process
-            saveData();
-        } else {
-            // Let's check whethere we already have the permission
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                // Permissions granted -> save the data
-                saveData();
-            } else {
-                // Android helper method to tell us if it's useful to show a hint
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    // Show some alert explaining why it is important to grant the permission
-                    showWritePermissionRationale(this);
-                } else {
-                    // Just straight to the point
-                    requestPermissions(
-                            new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            REQUEST_WRITE_PERMISSION);
-                }
-            }
-        }
+        // TODO: Check if you have the WRITE_EXTERNAL_STORAGE permission
     }
 
+    /**
+     * Use this method for displaying dialog with info about this permission.
+     */
     private void showWritePermissionRationale(final Context context) {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
         alertBuilder.setCancelable(true);
@@ -113,32 +79,14 @@ public class FileActivity
         alert.show();
     }
 
+    /**
+     * Save content of EditText to File
+     */
     private void saveData() {
         try {
             String content = mContentEditText.getText().toString();
 
-            File rootDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-            if (!rootDir.exists() && !rootDir.mkdirs()) {
-                Toast.makeText(FileActivity.this, "Couldn't create directory", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            File file = new File(rootDir, "seminar4.txt");
-            Log.i(TAG, file.getAbsolutePath());
-
-            // Make sure the file exists or create one
-            if (!file.exists()) {
-                if (!file.createNewFile()) {
-                    Toast.makeText(this, "Could not create the file", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-
-            // Writing the data
-            FileWriter fileWriter = new FileWriter(file, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(content);
-            bufferedWriter.close();
+            // TODO: Write content to File in Documents folder
 
             // Looks okay!
             Toast.makeText(this, "Saved.", Toast.LENGTH_SHORT).show();
