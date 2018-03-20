d4fb9f1abe8442c76956c07385ee7542446206e2
Jakub Turcovsky <jakub.turcovsky@dotykacka.cz>
2018-03-20 19:18:25 +0100

    Seminar 5 created

---
+++ PV239/seminar5/src/main/java/cz/pv239/seminar5/fragment/MainFragment.java
@@ -0,0 +1,39 @@
+package cz.pv239.seminar5.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import cz.pv239.seminar5.R;
+
+public class MainFragment
+        extends Fragment {
+
+    private static final String TAG = MainFragment.class.getSimpleName();
+
+    private Unbinder mUnbinder;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnbinder.unbind();
+    }
+}
