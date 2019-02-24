4e75195355d537433d87695a41f5419f948cb081
Jakub Turcovsky <jakubturcovsky@gmail.com>
2019-02-24 10:00:54 +0100

    Convert Java files to Kotlin

---
+++ PV239/seminar1/src/main/java/cz/pv239/seminar1/MainActivity.kt
@@ -0,0 +1,45 @@
+package cz.pv239.seminar1
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import cz.pv239.seminar1.MessageActivity.Companion.EXTRA_MESSAGE
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Set XML layout with views to this Activity
+        setContentView(R.layout.activity_main)
+
+        /* Exercise 1 */
+        // Get TextView with ID "text" from our layout
+        val text = findViewById<TextView>(R.id.text)
+        // Get Button with ID "button"
+        val button = findViewById<Button>(R.id.button)
+        // Listen for clicks on button
+        button.setOnClickListener {
+            Log.i(TAG, "User wants to know the answer")
+            text.setText(R.string.answer)
+        }
+
+        /* Exercise 2 */
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            Log.i(TAG, "User wants to send the message")
+            // Intent for starting a new MessageActivity
+            val intent = Intent(this, MessageActivity::class.java)
+            // If I want to put some info to the new Activity, I can put it into the Intent this way
+            intent.putExtra(EXTRA_MESSAGE, "Voilà")
+            // Start a MessageActivity
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
---
+++ PV239/seminar1/src/main/java/cz/pv239/seminar1/MessageActivity.kt
@@ -0,0 +1,26 @@
+package cz.pv239.seminar1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class MessageActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_MESSAGE = "message"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+
+        // Get the intent which started this Activity
+        val startingIntent = intent
+        // Get the extra content I've put to the Intent, in this case - String with message
+        val message = startingIntent.getStringExtra(EXTRA_MESSAGE)
+
+        // Set the message as a text to this Activity's TextView
+        val messageView = findViewById<TextView>(R.id.message)
+        messageView.text = message
+    }
+}
