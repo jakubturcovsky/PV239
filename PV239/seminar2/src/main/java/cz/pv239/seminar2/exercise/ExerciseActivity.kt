fbd8d36f8f828d25f4364b5b4434529892914d09
Jakub Turcovsky <jakubturcovsky@gmail.com>
2019-03-02 18:16:52 +0100

    Fix exercises made in the lecture

--- PV239/seminar2/src/main/java/cz/pv239/seminar2/exercise/ExerciseActivity.kt
+++ PV239/seminar2/src/main/java/cz/pv239/seminar2/exercise/ExerciseActivity.kt
@@ -2,6 +2,7 @@ package cz.pv239.seminar2.exercise
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import cz.pv239.seminar2.R
 
 // TODO Don't forget to register this activity in manifest!
 class ExerciseActivity : AppCompatActivity() {
@@ -9,11 +10,15 @@ class ExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set content view
+        setContentView(R.layout.activity_exercise)
 
         // Get FragmentManager
+        val fragmentManager = supportFragmentManager
 
         // Create Fragment
+        val fragment = ExerciseFragment()
 
         // Add Fragment to the FragmentManager, don't forget to commit
+        fragmentManager.beginTransaction().add(R.id.content,fragment).commit()
     }
 }
--- PV239/seminar2/src/main/java/cz/pv239/seminar2/exercise/ExerciseFragment.kt
+++ PV239/seminar2/src/main/java/cz/pv239/seminar2/exercise/ExerciseFragment.kt
@@ -1,26 +1,42 @@
 package cz.pv239.seminar2.exercise
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import cz.pv239.seminar2.R
+import cz.pv239.seminar2.exercise.ToastService.Companion.EXTRA_MESSAGE
+import kotlinx.android.synthetic.main.fragment_exercise.*
 
 class ExerciseFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        /* Exercise 1 */
-        // Inflating layout XML into a view
-        val view = inflater.inflate(R.layout.fragment_exercise, container, false)
 
+    /**
+     * Inflates your custom layout into the provided layout (container: ViewGroup?).
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflating layout XML into a view
+        // Third parameter should always be false, because FragmentManager handles attachment of the view, not you
+        // https://stackoverflow.com/a/45809756/2207083
+        return inflater.inflate(R.layout.fragment_exercise, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        /* Exercise 1 */
         // Add on button click listener
+        button.setOnClickListener { textview.text = edittext.text }
         // Inside it, get text from edittext and place it into textview
 
         /* Exercise 2 */
-        // Create Intent for starting ToastIntentService
-        // Put text from the edittext into the intent as extra (check ToastIntentService, for some name of the extra)
-        // start service using the created intent
-
-        return view
+        // Create Intent for starting ToastIntentServices
+        start_service.setOnClickListener {
+            val intent = Intent(context, ToastService::class.java)
+            // Put text from the edittext into the intent as extra (check ToastService for a name of the extra)
+            intent.putExtra(EXTRA_MESSAGE, edittext.text.toString())
+            // start service using the created intent
+            context?.startService(intent)
+        }
     }
 }
